c3c9e9490b4fef639681cdf4bf77c2c526632105
jvenicius <vidivinny8859@gmail.com>
2023-02-11 08:14:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primeiro Commit do App Fake News!
         </p>
         <a
           className="App-link"
